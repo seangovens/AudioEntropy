a88b328deb5f6a71ff6eeddd1c2ee3648c20effe
PSCucalgary <pscucalgary@gmail.com>
2016-11-29 18:57:15 -0700

    Added more measurements

--- src/project/Collector.java
+++ src/project/Collector.java
@@ -3,7 +3,7 @@ package project;
 import java.io.ByteArrayOutputStream;
 
 public class Collector {
-	static final int NUM_SEC = 120;
+	static final int NUM_SEC = 5;
 	
 	public static void main(String[] args) {
 		ByteArrayOutputStream in = new ByteArrayOutputStream();
@@ -24,6 +24,9 @@ public class Collector {
 		System.out.printf("\rSize of output: %d\n", in.size());
 		
 		Measurement m = new Measurement(in);
+		System.out.printf("Frequency Difference: %.6f\n", m.getFreqData());
+		System.out.printf("Runs test: %.6f\n", m.getRunsData());
 		System.out.printf("Naive min entropy: %.6f\n", m.getNaiveMinEntropy());
+		System.out.printf("Shannon entropy: %.6f\n", m.getShannonEntropy());
 	}
 }
--- src/project/Measurement.java
+++ src/project/Measurement.java
@@ -2,17 +2,28 @@ package project;
 
 import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Measurement {
 	int[] intData;
+	int numRuns = 0, num0 = 0, num1 = 0;
+	HashMap<Integer, Integer> freqs = new HashMap<>();
 	
 	public Measurement(ByteArrayOutputStream in) {
 		byte[] data = in.toByteArray();
 		intData = new int[data.length / 32];
 		
+		byte lastSym = 0;
 		for (int i = 0; i < data.length - (data.length % 32); i++) {
-			intData[i / 32] = intData[i / 32] ^ ((data[i] & 1) << (i % 32));
+			byte theBit = (byte) (data[i] & 1);
+			intData[i / 32] = intData[i / 32] ^ (theBit << (i % 32));
+			if (i > 0 && lastSym != theBit) numRuns++;
+			lastSym = theBit;
+			if (theBit == 1) num1++;
+			else num0++;
 		}
+		
+		calcFreqs(freqs);
 		
 		/*for (int i = 0; i < data.length; i+=4) {
 			int ind = i / 4;
@@ -23,11 +34,10 @@ public class Measurement {
 		}*/
 	}
 	
-	public double getNaiveMinEntropy() {
-		double res = 0.0;
-		int currMaxInt = 0; int currMaxFreq = 0;
-		HashMap<Integer, Integer> freqs = new HashMap<>();
-		
+	int currMaxFreq;
+	
+	public void calcFreqs(HashMap<Integer, Integer> freqs) {
+		int currMaxInt = 0; currMaxFreq = 0;
 		for (int i = 0; i < intData.length; i++) {
 			int currFreq = 0;
 			if (!freqs.containsKey(intData[i]))
@@ -42,10 +52,39 @@ public class Measurement {
 			
 			freqs.put(intData[i], currFreq);
 		}
-		
+	}
+	
+	public double getNaiveMinEntropy() {
+		double res = 0.0;
 		res = -Math.log10((double) currMaxFreq / intData.length) / Math.log10(2.0);
-		System.out.printf("Max frequency symbol %s with frequency %d\n", Integer.toBinaryString(currMaxInt), freqs.get(currMaxInt));
+		//System.out.printf("Max frequency symbol %s with frequency %d\n", Integer.toBinaryString(currMaxInt), freqs.get(currMaxInt));
 		
 		return res;
+	}
+	
+	public double getShannonEntropy() {
+		double theShannon = 0.0;
+		for (Map.Entry<Integer, Integer> e : freqs.entrySet()) {
+			double prob = (double) e.getValue() / intData.length;
+			theShannon += -prob * (Math.log10(prob) / Math.log10(2.0));
+		}
+		
+		return theShannon;
+	}
+	
+	public double getRunsData() {
+		double eRuns = ((2.0 * num0 * num1) / ((double) num0 + num1)) + 1.0;
+		double var = (2.0 * num0 * num1 * ((2.0 * num0 * num1) - num0 - num1));
+		var /= (double) Math.pow(num1 + num0, 2) * (num0 + num1 - 1.0);
+		double sDev = Math.sqrt(var);
+		
+		return Math.abs((numRuns - eRuns) / sDev); 
+	}
+	
+	public double getFreqData() {
+		double zeroPer = (double) num0 / (num0 + num1);
+		double onePer = (double) num1 / (num0 + num1);
+		
+		return Math.abs(zeroPer - onePer);
 	}
 }
